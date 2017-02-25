b61ae7d6e578a96990de36511556d30c6783ae16
Sebastian Rolf <basti@rolf-dv.de>
2017-02-25 16:06:14 +0100

    initial stepper settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -487,7 +487,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {100.00,100.00,1600.00,760*1.1}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.00, 80.00, 1600.00, 120.00}
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
